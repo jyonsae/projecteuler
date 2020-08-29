5d00cdb3855ab73e7acc2c7727f1f78a790706cf
Furkan <66871748+jyonsae@users.noreply.github.com>
2020-08-29 21:34:30 +0300

    Update problem_3.py

--- problem_3.py
+++ problem_3.py
@@ -1,15 +1,22 @@
 my_number = 600851475143
+largestNumber = 0
 
 
 def get_factor(number):
     num_arr = []
     for i in range(1, number + 1):
         if number % i == 0:
-            for j in range(1, 10):
-                if number % i != 0:
-                    num_arr.append(i)
+            num_arr.append(i)
 
     return num_arr
 
 
-print(get_factor(50))
+def is_prime(number):
+    return len(get_factor(number)) == 2
+
+
+for i in get_factor(my_number):
+    if is_prime(i) and i > largestNumber:
+        largestNumber = i
+
+print(largestNumber)
